f2b6b328bb1d92e6ef1ea58e884742a3156a9e8b
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2023-01-02 14:37:42 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1 className='text-3xl font-semibold text-emerald-500'> Hello Diet Chart APP</h1>
+    </>
+  );
+}
+
+export default App;
