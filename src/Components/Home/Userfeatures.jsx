b7b009902a320a666691f1f47f1ebc8e5acca855
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2023-01-28 11:29:03 +0530

    Specifaction Section with SCSS added

--- src/Components/Home/Userfeatures.jsx
+++ src/Components/Home/Userfeatures.jsx
@@ -19,53 +19,53 @@ const features = [
 const Userfeatures = () => {
   return (
     <>
-      <div className="bg-white">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+      <div className="userspec__Bg--White">
+        <div className="userspec__Container">
           <div>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            <h2 className="userspec__Heading--Primary">
               Technical Specifications
             </h2>
-            <p className="mt-4 text-gray-500">
+            <p className="userspec__Heading--Info">
               The walnut wood card tray is precision milled to perfectly fit a
               stack of Focus cards. The powder coated steel divider separates
               active cards from new ones, or can be used to archive important
               task lists.
             </p>
 
-            <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+            <dl className="userspec__Grid--Left">
               {features.map((feature) => (
                 <div
                   key={feature.name}
-                  className="border-t border-gray-200 pt-4"
+                  className="userspec__Border--Top"
                 >
-                  <dt className="font-medium text-gray-900">{feature.name}</dt>
-                  <dd className="mt-2 text-sm text-gray-500">
+                  <dt className="userspec__Grid--Left-Title">{feature.name}</dt>
+                  <dd className="userspec__Grid--Left-Info">
                     {feature.description}
                   </dd>
                 </div>
               ))}
             </dl>
           </div>
-          <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+          <div className="userspec__Grid--Right">
             <img
               src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
               alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
-              className="rounded-lg bg-gray-100"
+              className="userspec__Grid--Right-Img"
             />
             <img
               src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
               alt="Top down view of walnut card tray with embedded magnets and card groove."
-              className="rounded-lg bg-gray-100"
+              className="userspec__Grid--Right-Img"
             />
             <img
               src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
               alt="Side of walnut card tray with card groove and recessed card area."
-              className="rounded-lg bg-gray-100"
+              className="userspec__Grid--Right-Img"
             />
             <img
               src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
               alt="Walnut card tray filled with cards and card angled in dedicated groove."
-              className="rounded-lg bg-gray-100"
+              className="userspec__Grid--Right-Img"
             />
           </div>
         </div>
