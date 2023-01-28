1bb7346227eca7a7ee5f8b48bf2021b347d2c6a6
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2023-01-28 11:16:57 +0530

    Getstartted SCSS added

--- src/Components/Home/GetStarted.jsx
+++ src/Components/Home/GetStarted.jsx
@@ -3,27 +3,27 @@ import React from "react";
 const GetStarted = () => {
   return (
     <>
-      <div className="bg-gray-50">
-        <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            <span className="block">Ready to dive in?</span>
-            <span className="block text-indigo-600">
+      <div className="getstart__Bg--Gray">
+        <div className="getstart__Container">
+          <h2 className="getstart__Heading">
+            <span className="getstart__Heading--Secondary">Ready to dive in?</span>
+            <span className="getstart__Heading--Primary">
               Start your free trial today.
             </span>
           </h2>
-          <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-            <div className="inline-flex rounded-md shadow">
+          <div className="getstart__Flex--Container">
+            <div className="getstart__Flex--Inline">
               <a
                 href="/"
-                className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700"
+                className="getstart__Flex--Items-Primary"
               >
                 Get started
               </a>
             </div>
-            <div className="ml-3 inline-flex rounded-md shadow">
+            <div className="getstart__Flex--Gap-1 getstart__Flex--Inline">
               <a
                 href="/"
-                className="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50"
+                className="getstart__Flex--Items-Secondary"
               >
                 Learn more
               </a>
