8f24234323afa5933c072d0d0c9afec43cdb4bc1
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2023-01-28 11:03:34 +0530

    Feature SCSS Added

--- src/Components/Home/Features.jsx
+++ src/Components/Home/Features.jsx
@@ -31,36 +31,36 @@ const features = [
 const Features = () => {
   return (
     <>
-      <div className="bg-white py-24 sm:py-32 lg:py-40">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="sm:text-center">
-            <h2 className="text-lg font-semibold leading-8 text-indigo-600">
+      <div className="feature__Container">
+        <div className="feature__Auto">
+          <div className="feature__Block--Center">
+            <h2 className="feature__Heading--Primary-Sub">
               Transactions
             </h2>
-            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            <p className="feature__Heading--Primary">
               A better way to send money
             </p>
-            <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600">
+            <p className="feature__Heading--Info">
               Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
               magnam voluptatum cupiditate veritatis in accusamus quisquam.
             </p>
           </div>
 
-          <div className="mt-20 max-w-lg sm:mx-auto md:max-w-none">
-            <div className="grid grid-cols-1 gap-y-16 md:grid-cols-2 md:gap-x-12 md:gap-y-16">
+          <div className="feature__Box">
+            <div className="feature__Grids">
               {features.map((feature) => (
                 <div
                   key={feature.name}
-                  className="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row"
+                  className="feature__Flex--Box"
                 >
-                  <div className="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white sm:shrink-0">
-                    <feature.icon className="h-8 w-8" aria-hidden="true" />
+                  <div className="feature__Flex--Item">
+                    <feature.icon className="feature__Flex--Icon" aria-hidden="true" />
                   </div>
-                  <div className="sm:min-w-0 sm:flex-1">
-                    <p className="text-lg font-semibold leading-8 text-gray-900">
+                  <div className="feature__Flex--Heading">
+                    <p className="feature__Flex--Heading-Primary">
                       {feature.name}
                     </p>
-                    <p className="mt-2 text-base leading-7 text-gray-600">
+                    <p className="feature__Flex--Heading-Info">
                       {feature.description}
                     </p>
                   </div>
